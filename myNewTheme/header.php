ce1884afa67dca8ba54551923476a29742c4ae90
ReyMArias <57506826+ReyMArias@users.noreply.github.com>
2021-10-01 21:40:21 -0400

    Add files via upload

---
+++ myNewTheme/header.php
@@ -0,0 +1,36 @@
+<html>
+
+<head>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
+    <title>Tutorial theme</title>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() . '/js/jquery.js'; ?>">
+    </script>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() . '/js/jquery-ui.min.js'; ?>">
+    </script>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() . '/js/bootstrap.js'; ?>">
+    </script>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() . '/css/bootstrap.css'; ?>">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+
+
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="profile" href="https://gmpg.org/xfn/11" />
+	<?php wp_head(); ?>
+
+</head>
+
+<body>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+    <div id="ttr_header" class="jumbotron">
+        <h1>HEADER</h1>
+    </div>
+    <div class="container">
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
